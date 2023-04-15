abb8cd3e4f44916e0fa66e28701a80cfc2735778
Michal Tvrznik <emporeor@gmail.com>
2023-04-15 13:00:51 +0200

    Work with absolute path from config.

--- config.go
+++ config.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/tvrzna/go-utils/args"
@@ -22,7 +24,6 @@ func loadConfig(arg []string) *config {
 	c := &config{cwd, "", 1500, layout()}
 
 	args.ParseArgs(arg, func(arg, value string) {
-	args.ParseArgs(os.Args, func(arg, value string) {
 		switch arg {
 		case "-h", "--help":
 			fmt.Printf("Usage: mkwiki [options]\nOptions:\n\t-h, --help\t\t\tprint this help\n\t-v, --version\t\t\tprint version\n\t-t, --path [PATH]\t\tabsolute path to markdown storage\n\t-p, --port [PORT]\t\tsets port for listening\n\t-a, --app-url [APP_URL]\t\tapplication url (if behind proxy)\n")
@@ -31,7 +32,11 @@ func loadConfig(arg []string) *config {
 			fmt.Printf("mkwiki 0.1.0\nhttps://github.com/tvrzna/mkwiki\n\nReleased under the MIT License.\n")
 			os.Exit(0)
 		case "-t", "--path":
-			c.path = value
+			if path, err := filepath.Abs(value); err != nil {
+				log.Fatal("wrong path", err)
+			} else {
+				c.path = path
+			}
 		case "-p", "--port":
 			c.port, _ = strconv.Atoi(value)
 		case "-a", "--app-url":
